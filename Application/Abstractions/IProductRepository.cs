ca3c6903ed01f906a659cfd802104b14bea07313
Roger Waldron <87526672+RogerWaldron@users.noreply.github.com>
2023-11-02 08:24:37 +0400

    Application (#3)
    * Created Product interface
    
    * fixup! import Domain.Model and create reference
    
    ---------
    
    Co-authored-by: rwaldron <outsideinprofits@gmail.com>

---
+++ Application/Abstractions/IProductRepository.cs
@@ -0,0 +1,17 @@
+using Domain.Models;
+
+namespace Application.Abstraction
+{
+    public interface IProductRepository
+    {
+        Task<ICollection<Product>> GetAllProducts();
+
+        Task<Product> GetProductById(int productId);
+
+        Task<Product> CreateProduct(Product toCreate);
+
+        Task<Product> UpdateProduct(Product updated, int productId);
+
+        Task DeleteProduct(int productId);
+    }
+}
