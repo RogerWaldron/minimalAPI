69583a41bb11aff8aab40be78e060b7494a99d29
rwaldron <outsideinprofits@gmail.com>
2023-11-02 15:00:49 +1100

    Created ApplicationDbContext

---
+++ DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions opt) : base(opt)
+        {
+             
+        }
+        
+        public DbSet<Product> Product { get; set; } 
+    }
+}
+
