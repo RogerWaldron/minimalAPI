a1bfdef23cfb8fd05372496babf86f4231fad25e
rwaldron <outsideinprofits@gmail.com>
2023-11-04 13:29:08 +1100

    Added services for DBContext, ProductRepository and reference to DataAccess

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,19 @@
+using Application.Abstraction;
+using DataAccess.Context;
+using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Config;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.RegisterServices();
+var cs = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<ApplicationDbContext>(
+    opt => opt.UseSqlServer(cs));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 var app = builder.Build();
 
+app.RegisterEndpoints();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
