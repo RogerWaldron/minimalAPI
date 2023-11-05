fdc5450aaf30a0b3c052f0b67a3b6297e3047c20
rwaldron <outsideinprofits@gmail.com>
2023-11-05 16:28:52 +1100

    Refactored Services into extension method RegisterServices

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,15 +1,8 @@
-using Application.Abstraction;
-using DataAccess.Context;
-using DataAccess.Repositories;
-using Microsoft.EntityFrameworkCore;
 using WebApi.Config;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.RegisterServices();
-var cs = builder.Configuration.GetConnectionString("Default");
-builder.Services.AddDbContext<ApplicationDbContext>(
-    opt => opt.UseSqlServer(cs));
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 
 var app = builder.Build();
 
