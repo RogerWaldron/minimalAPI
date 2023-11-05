a7537e0421c0b67bedf870cfd01580123efea450
rwaldron <outsideinprofits@gmail.com>
2023-11-05 22:51:31 +1100

    Register Mediatr service

--- WebApi/Config/MinimalApiExtensions.cs
+++ WebApi/Config/MinimalApiExtensions.cs
@@ -1,4 +1,7 @@
+using System.Net.Mime;
+using System.Reflection;
 using Application.Abstraction;
+using Application.Products.CommandHandlers;
 using DataAccess.Context;
 using DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -11,7 +14,8 @@ namespace WebApi.Config
         {
             var cs = builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("DB connection string not found!");
             builder.Services.AddDbContext<ProductDbContext>(opt => opt.UseSqlServer(cs));
-            builder.Services.AddScoped<IProductRepository, ProductRepository>();           
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+            builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblies(typeof(CreateProductHandler).GetTypeInfo().Assembly));
         }
 
         public static void RegisterEndpoints(this WebApplication app)
