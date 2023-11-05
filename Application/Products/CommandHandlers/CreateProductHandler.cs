a6cc2f2c88670e321679d91b5594b7747f06f0bb
rwaldron <outsideinprofits@gmail.com>
2023-11-05 19:17:10 +1100

    Added handlers for Create, Delete and Update of Product

---
+++ Application/Products/CommandHandlers/CreateProductHandler.cs
@@ -0,0 +1,28 @@
+using Application.Abstraction;
+using Application.Products.Commands;
+using Domain.Models;
+using MediatR;
+
+namespace Application.Products.CommandHandlers;
+
+public class CreateProductHandler : IRequestHandler<CreateProduct, Product>
+{
+    private readonly IProductRepository _productRepository;
+
+    public CreateProductHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<Product> Handle(CreateProduct request, CancellationToken cancellationToken)
+    {
+        var newProduct = new Product
+        {
+            Brand = request.Brand,
+            Title = request.Title,
+            Description = request.Description
+        };
+
+        return await _productRepository.CreateProduct(newProduct);
+    }
+}
