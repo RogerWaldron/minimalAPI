7f7fed4111bb1755e4ccbd7f7fe1c08e0a796c45
rwaldron <outsideinprofits@gmail.com>
2023-11-06 19:39:11 +1100

    Added dependency Swagger & Swashbuckle

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,11 +1,23 @@
 using WebApi.Config;
 
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.RegisterMyServices();
 
 
 var app = builder.Build();
 
 app.RegisterProductsApiEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
 
 app.Run();
