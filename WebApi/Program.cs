248cf823b5ed170465d5bdc324f4de2daf439110
rwaldron <outsideinprofits@gmail.com>
2023-11-06 20:03:21 +1100

    Added dependencies for ApiVersioning

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,8 +1,20 @@
+using Asp.Versioning;
 using WebApi.Config;
 
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApiVersioning(opts =>
+{
+    opts.DefaultApiVersion = new ApiVersion(1.0);
+    opts.AssumeDefaultVersionWhenUnspecified = true;
+    opts.ApiVersionReader = new UrlSegmentApiVersionReader();
+}).AddApiExplorer(opts =>
+{
+    opts.GroupNameFormat = "'v'VVV";
+    opts.SubstituteApiVersionInUrl = true;
+}).EnableApiVersionBinding();
+
 builder.Services.AddSwaggerGen();
 
 builder.RegisterMyServices();
