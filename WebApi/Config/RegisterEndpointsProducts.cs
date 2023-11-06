c42b740034a3b89ed4b8ccff75e8f3301f1e0975
rwaldron <outsideinprofits@gmail.com>
2023-11-06 15:04:58 +1100

    Refactored endpoint logic into private static functions

--- WebApi/Config/RegisterEndpointsProducts.cs
+++ WebApi/Config/RegisterEndpointsProducts.cs
@@ -3,62 +3,71 @@ using Application.Products.Queries;
 using Domain.Models;
 using MediatR;
 
-namespace WebApi.Config
+namespace WebApi.Config;
+
+public static class RegisterEndpointsProducts
 {
-    public static class RegisterEndpointsProducts
+    public static void RegisterProductsApiEndpoints(this WebApplication app)
     {
-        public static void RegisterProductsApiEndpoints(this WebApplication app)
+        var groupUrl = app.MapGroup("/api/products");
+
+        groupUrl.MapGet("/{id}", GetProductByIdAsync).WithName("GetProductById");
+
+        groupUrl.MapGet("/", GetAllProductsAsync);
+
+        groupUrl.MapPost("/", CreateProductAsync);
+
+        groupUrl.MapPut("/{id}", UpdateProductAsync);
+
+        groupUrl.MapDelete("/{id}", DeleteProductAsync);
+    }
+
+    private static async Task<IResult> GetAllProductsAsync(IMediator m)
+    {
+        var req = new GetAllProducts();
+        var products = await m.Send(req);
+
+        return Results.Ok(products);
+    }
+
+    private static async Task<IResult> GetProductByIdAsync(IMediator m, int id)
+    {
+        var getProduct = new GetProductById { ProductId = id };
+        var result = await m.Send(getProduct);
+
+        return Results.Ok(result);
+    }
+
+    private static async Task<IResult> CreateProductAsync(IMediator m, Product product)
+    {
+        var createProduct = new CreateProduct
         {
-            var groupUrl = app.MapGroup("/api/products");
-            
-            groupUrl.MapGet("/{id}", async (IMediator m, int id) =>
-            {
-                var getProduct = new GetProductById { ProductId = id};
-                var result = await m.Send(getProduct);
+            Brand = product.Brand, Title = product.Title, Description = product.Description
+        };
+        var newProduct = await m.Send(createProduct);
+        
+        return Results.CreatedAtRoute("GetProductById", new { newProduct.Id }, newProduct);
+    }
 
-                return Results.Ok(result);
-            }).WithName("GetProductById");
+    private static async Task<IResult> UpdateProductAsync(IMediator m, Product product, int id)
+    {
+        var update = new UpdateProduct
+        {
+            ProductId = id,
+            Brand = product.Brand,
+            Title = product.Title,
+            Description = product.Description
+        };
+        var updatedProduct = await m.Send(update);
 
-            groupUrl.MapGet("/", async (IMediator m) =>
-            {
-                var req = new GetAllProducts();
-                var products = await m.Send(req);
-                
-                return Results.Ok(products);
-            });
+        return Results.Ok(updatedProduct);
+    }
 
-            groupUrl.MapPost("/", async (IMediator m, Product product) =>
-            {
-                var createProduct = new CreateProduct
-                {
-                    Brand = product.Brand, Title = product.Title, Description = product.Description
-                };
-                var newProduct = await m.Send(createProduct);
+    private static async Task<IResult> DeleteProductAsync(IMediator m, int id)
+    {
+        var delete = new DeleteProduct { ProductId = id };
+        await m.Send(delete);
 
-                return Results.CreatedAtRoute("GetProductById", new { newProduct.Id }, newProduct);
-            });
-
-            groupUrl.MapPut("/{id}", async (IMediator m, Product product, int id) =>
-            {
-                var update = new UpdateProduct
-                {
-                    ProductId = id,
-                    Brand = product.Brand,
-                    Title = product.Title,
-                    Description = product.Description
-                };
-                var updatedProduct = await m.Send(update);
-
-                return Results.Ok(updatedProduct);
-            });
-
-            groupUrl.MapDelete("/{id}", async (IMediator m, int id) =>
-            {
-                var delete = new DeleteProduct { ProductId = id };
-                await m.Send(delete);
-
-                return Results.NoContent();
-            });
-        }
+        return Results.NoContent();
     }
 }
