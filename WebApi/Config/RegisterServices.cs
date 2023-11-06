24a3695212af48b3e6de68c0aa19d6636488aa23
rwaldron <outsideinprofits@gmail.com>
2023-11-06 11:10:03 +1100

    Created Endpoints for GetAllProducts, Post, Put, Delete
    Refactored Registering of Endpoints and Services into separate files.

--- WebApi/Config/RegisterServices.cs
+++ WebApi/Config/RegisterServices.cs
@@ -1,4 +1,3 @@
-using System.Net.Mime;
 using System.Reflection;
 using Application.Abstraction;
 using Application.Products.CommandHandlers;
@@ -8,19 +7,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WebApi.Config
 {
-    public static class MinimalApiExtensions
+    public static class RegisterServices
     {
-        public static void RegisterServices(this WebApplicationBuilder builder)
+        public static void RegisterMyServices(this WebApplicationBuilder builder)
         {
             var cs = builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("DB connection string not found!");
             builder.Services.AddDbContext<ProductDbContext>(opt => opt.UseSqlServer(cs));
             builder.Services.AddScoped<IProductRepository, ProductRepository>();
             builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblies(typeof(CreateProductHandler).GetTypeInfo().Assembly));
-        }
-
-        public static void RegisterEndpoints(this WebApplication app)
-        {
-            
         }
     }
 }
