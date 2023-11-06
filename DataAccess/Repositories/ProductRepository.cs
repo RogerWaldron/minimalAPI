35eecd514fd07b78993fb5d499b09c6650a80e5f
rwaldron <outsideinprofits@gmail.com>
2023-11-06 19:06:44 +1100

    Removed nullable

--- DataAccess/Repositories/ProductRepository.cs
+++ DataAccess/Repositories/ProductRepository.cs
@@ -1,65 +1,64 @@
-using Application.Abstraction;
+using Application.Abstractions;
 using DataAccess.Context;
 using Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace DataAccess.Repositories
+namespace DataAccess.Repositories;
+
+public class ProductRepository : IProductRepository
 {
-    public class ProductRepository : IProductRepository
+    private readonly ProductDbContext _ctx;
+    
+    public ProductRepository(ProductDbContext ctx)
     {
-        private readonly ProductDbContext _ctx;
+        _ctx = ctx;
+    }
+
+    public async Task<ICollection<Product>> GetAllProducts()
+    {
+        return await _ctx.Products.ToListAsync();
+    }
+
+    public async Task<Product> GetProductById(int productId)
+    {
+        return await _ctx.Products.FirstOrDefaultAsync(p => p.Id == productId);
+    }
+
+    public async Task<Product> CreateProduct(Product toCreate)
+    {
+        toCreate.DateCreated = DateTime.Now;
+        toCreate.LastModified = DateTime.Now;
         
-        public ProductRepository(ProductDbContext ctx)
-        {
-            _ctx = ctx;
-        }
+        _ctx.Products.Add(toCreate);
+        await _ctx.SaveChangesAsync();
+        
+        return toCreate;
+    }
 
-        public async Task<ICollection<Product>> GetAllProducts()
-        {
-            return await _ctx.Products.ToListAsync();
-        }
+    public async Task<Product> UpdateProduct(Product toUpdate, int productId)
+    {
+        var product = await _ctx.Products.FirstOrDefaultAsync(p => p.Id == productId);
 
-        public async Task<Product?> GetProductById(int productId)
-        {
-            return await _ctx.Products.FirstOrDefaultAsync(p => p.Id == productId);
-        }
+        if (product is null) return product;
+        
+        product.LastModified = DateTime.Now;
+        product.Brand = toUpdate.Brand;
+        product.Title = toUpdate.Title;
+        product.Description = toUpdate.Description;
 
-        public async Task<Product> CreateProduct(Product toCreate)
-        {
-            toCreate.DateCreated = DateTime.Now;
-            toCreate.LastModified = DateTime.Now;
-            
-            _ctx.Products.Add(toCreate);
-            await _ctx.SaveChangesAsync();
-            
-            return toCreate;
-        }
+        _ctx.Products.Update(product);
+        await _ctx.SaveChangesAsync();
 
-        public async Task<Product?> UpdateProduct(Product toUpdate, int productId)
-        {
-            var product = await _ctx.Products.FirstOrDefaultAsync(p => p.Id == productId);
+        return product;
+    }
 
-            if (product is null) return product;
-            
-            product.LastModified = DateTime.Now;
-            product.Brand = toUpdate.Brand;
-            product.Title = toUpdate.Title;
-            product.Description = toUpdate.Description;
+    public async Task DeleteProduct(int productId)
+    {
+        var product = await _ctx.Products.FirstOrDefaultAsync(p => p.Id == productId);
 
-            _ctx.Products.Update(product);
-            await _ctx.SaveChangesAsync();
+        if (product is null) return;
 
-            return product;
-        }
-
-        public async Task DeleteProduct(int productId)
-        {
-            var product = await _ctx.Products.FirstOrDefaultAsync(p => p.Id == productId);
-
-            if (product is null) return;
-
-            _ctx.Products.Remove(product);
-            await _ctx.SaveChangesAsync();
-        }
+        _ctx.Products.Remove(product);
+        await _ctx.SaveChangesAsync();
     }
 }
